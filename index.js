4723afa2509de13d830e81b3760b58135a5e0a8b
Leestan360 <stanleymayore2@gmail.com>
2022-06-11 14:00:40 +0300

    commit tree 1ad61ca5322302e6a188e9f1b9c56551455cb5b8

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    const driversArr = drivers.filter(
+        (match) => match.toLowerCase() === name.toLowerCase());
+    return driversArr;
+}
+
+function fuzzyMatch(drivers, name) {
+    const driversArr = drivers.filter(
+        (match) => match.toLowerCase().indexOf(name.toLowerCase()) === 0
+    )
+    return driversArr;
+}
+
+function matchName(driver, name) {
+    const driverArr = driver.filter(
+        (match) => match.name === name
+    )
+    return driverArr;
+
+}
